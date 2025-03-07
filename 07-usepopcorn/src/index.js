d3ab353cba19f2dea9756f3feed4f4c2570e9633
sonambharti <subodh9955509176@gmail.com>
2025-03-07 16:42:44 +0530

    Adding usepopcorn working app-v2

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App-v1';
+// import App from './App-v1';
+import App from './App-v2';
+// import App from './App-v3';
 // import App from './Components/App';
 // import StarRating from './Components/StarRating';
 
